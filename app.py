df1372d54565d8355ad3f8ae009caf4afb00cd67
kleber-code <kleber.code@gmail.com>
2023-04-22 15:48:17 -0300

    fix pythonanywhere start problems

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ load_dotenv()
 
 app = Flask(__name__)
 
+import hydra, etk
+app.register_blueprint(hydra.bp)
+app.register_blueprint(etk.bp)
+
 @app.route('/')
 def home():
     return "Welcome to Dragon"
 
-if __name__ == '__main__':
-    import hydra, etk
-    app.register_blueprint(hydra.bp)
-    app.register_blueprint(etk.bp)
-    app.run(debug=True)
+@app.route('/url_map')
+def url_map():
+    return app.url_map
